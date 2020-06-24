2b0f5d83a73ba4a62449f4f6d097afaaa29b7cde
Herick Portugues <herick.portugues@gmail.com>
2020-06-24 12:57:07 -0300

    Use the Story Plan to progress through the story

--- lib/story_brain.dart
+++ lib/story_brain.dart
@@ -47,14 +47,39 @@ class StoryBrain {
 
   String getChoice2() => _storyData.first.choice2;
 
-  void nextStory(int choiceNumber) {}
+  void nextStory(int choiceNumber) {
+    switch (storyNumber) {
+      case 0:
+        if (choiceNumber == 1) {
+          storyNumber = 2;
+        } else if (choiceNumber == 2) {
+          storyNumber = 1;
+        }
+        break;
+      case 1:
+        if (choiceNumber == 1) {
+          storyNumber = 2;
+        } else if (choiceNumber == 2) {
+          storyNumber = 3;
+        }
+        break;
+      case 2:
+        if (choiceNumber == 1) {
+          storyNumber = 5;
+        } else if (choiceNumber == 2) {
+          storyNumber = 5;
+        }
+        break;
+      default:
+        storyNumber = 0;
+        break;
+    }
+  }
 }
 
 //TODO: Step 23 - Use the storyNumber property inside getStory(), getChoice1() and getChoice2() so that it gets the updated story and choices rather than always just the first (0th) one.
 
 //TODO: Step 25 - Change the storyNumber property into a private property so that only story_brain.dart has access to it. You can do this by right clicking on the name (storyNumber) and selecting Refactor -> Rename to make the change across all the places where it's used.
-
-//TODO: Step 20 - Download the story plan here: https://drive.google.com/uc?export=download&id=1KU6EghkO9Hf2hRM0756xFHgNaZyGCou3
 
 //TODO: Step 21 - Using the story plan, update nextStory() to change the storyNumber depending on the choice made by the user. e.g. if choiceNumber was equal to 1 and the storyNumber is 0, the storyNumber should become 2.
 
